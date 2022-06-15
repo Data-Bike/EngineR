e72b17f90fd2458f578647a33a41cab424ea17e6
gorod <gorodmehov@gmail.com>
2022-06-15 22:25:33 +0300

    Исользуем Future из crate futures для стабильной работы между потоками и реализацией трейта send

--- src/model/lfu_cache/cache.rs
+++ src/model/lfu_cache/cache.rs
@@ -4,8 +4,9 @@ use crate::model::object::entity::object::{Object, ObjectType};
 use crate::model::secure::entity::permission::{Group};
 use crate::model::user::entity::user::User;
 use lazy_static::lazy_static;
-use std::sync::Mutex;
+// use std::sync::Mutex;
 use crate::model::link::entity::link::LinkType;
+use futures::lock::Mutex;
 
 pub struct LFU {
     pub user_by_id: WTinyLFUCache<String, User, DefaultKeyHasher<String>>,
@@ -37,15 +38,15 @@ lazy_static! {
    pub static ref CACHE:Mutex<LFU> = Mutex::new(LFU::new(1000));
 }
 
-pub fn clear_cache() {
-    CACHE.lock().unwrap().user_by_id.purge();
-    CACHE.lock().unwrap().group_by_id.purge();
-    CACHE.lock().unwrap().link_type_by_alias.purge();
-    CACHE.lock().unwrap().link_type_by_id.purge();
-    CACHE.lock().unwrap().user_by_login.purge();
-    CACHE.lock().unwrap().object_type_by_id.purge();
-    CACHE.lock().unwrap().object_type_by_alias.purge();
-    CACHE.lock().unwrap().object_by_id.purge();
+pub async fn clear_cache() {
+    CACHE.lock().await.user_by_id.purge();
+    CACHE.lock().await.group_by_id.purge();
+    CACHE.lock().await.link_type_by_alias.purge();
+    CACHE.lock().await.link_type_by_id.purge();
+    CACHE.lock().await.user_by_login.purge();
+    CACHE.lock().await.object_type_by_id.purge();
+    CACHE.lock().await.object_type_by_alias.purge();
+    CACHE.lock().await.object_by_id.purge();
 }
 
 #[macro_export]
@@ -54,7 +55,7 @@ macro_rules! cache_it {
                 {
                 use crate::model::lfu_cache::cache::CACHE;
                 use caches::Cache;
-                Ok(match CACHE.lock().unwrap().$cache.get($key) {
+                Ok(match CACHE.lock().await.$cache.get($key) {
                     None => {
                         $x
                     }
@@ -69,7 +70,7 @@ macro_rules! remove_it_from_cache {
         {
                 use crate::model::lfu_cache::cache::CACHE;
                 use caches::Cache;
-                CACHE.lock().unwrap().$cache.remove($key) 
+                CACHE.lock().await.$cache.remove($key)
         }
     };
 }
