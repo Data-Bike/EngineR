e0af6c8e749c886bfd73b0523b303985a14cea21
gorod <gorodmehov@gmail.com>
2022-06-26 17:07:35 +0300

    Тесты контроллеров переведены на хелпер. В хелпере тестов контроллеров прописано создание случайных пользователей с случайными алиасами прав. Добавлены настройки контейнера постгреса в настройки проекта

--- src/controllers/link.rs
+++ src/controllers/link.rs
@@ -68,6 +68,7 @@ mod test {
     use rocket::log::private::kv::Source;
     use rocket::uri;
     use crate::{rocket_build};
+    use crate::controllers::test::login;
     use crate::model::error::RepositoryError;
     use crate::model::object::entity::object::{Field, ObjectType};
     use crate::model::secure::entity::permission::{Access, Group, Permission, PermissionKind, PermissionLevel, PermissionsGroup};
@@ -85,216 +86,12 @@ mod test {
         assert_eq!(response.into_string().unwrap(), "Hello!");
     }
 
-    fn add_object_type() {
-        match block_on(Object_Repository::getObjectTypeFromAlias("fl".to_string())) {
-            Ok(_) => { return; }
-            Err(_) => {}
-        };
-
-        match block_on(Object_Repository::createObjectType(ObjectType {
-            id: None,
-            fields: vec![
-                Field {
-                    id: None,
-                    alias: "lastname".to_string(),
-                    kind: "varchar(255)".to_string(),
-                    name: "lastname".to_string(),
-                    default: None,
-                    value: None,
-                    require: false,
-                    index: false,
-                    preview: false,
-                },
-                Field {
-                    id: None,
-                    alias: "firstname".to_string(),
-                    kind: "varchar(255)".to_string(),
-                    name: "firstname".to_string(),
-                    default: None,
-                    value: None,
-                    require: false,
-                    index: false,
-                    preview: false,
-                },
-                Field {
-                    id: None,
-                    alias: "patronymic".to_string(),
-                    kind: "varchar(255)".to_string(),
-                    name: "patronymic".to_string(),
-                    default: None,
-                    value: None,
-                    require: false,
-                    index: false,
-                    preview: false,
-                },
-                Field {
-                    id: None,
-                    alias: "birthday".to_string(),
-                    kind: "timestamp".to_string(),
-                    name: "birthday".to_string(),
-                    default: None,
-                    value: None,
-                    require: false,
-                    index: false,
-                    preview: false,
-                },
-            ],
-            kind: "object".to_string(),
-            alias: "fl".to_string(),
-        })) {
-            Ok(_) => {}
-            Err(e) => {
-                println!("{:?}", e);
-                panic!("Error create object type");
-            }
-        };
-    }
-
-
-    fn get_user_groups() -> Vec<Group> {
-        let g = Group {
-            alias: "acc_object".to_string(),
-            name: "acc_object".to_string(),
-            level: "system".to_string(),
-            id: Some("1".to_string()),
-            permissions: PermissionsGroup {
-                system: vec![Permission {
-                    access: Access::allow,
-                    alias: "system_of_object_access".to_string(),
-                    id: None,
-                    level: PermissionLevel::system,
-                    kind: PermissionKind::create,
-                    name: "system_of_object_access".to_string(),
-                    object: "object".to_string(),
-                },Permission {
-                    access: Access::allow,
-                    alias: "system_of_object_access".to_string(),
-                    id: None,
-                    level: PermissionLevel::system,
-                    kind: PermissionKind::create,
-                    name: "system_of_object_access".to_string(),
-                    object: "user".to_string(),
-                },Permission {
-                    access: Access::allow,
-                    alias: "system_of_object_access".to_string(),
-                    id: None,
-                    level: PermissionLevel::system,
-                    kind: PermissionKind::create,
-                    name: "system_of_object_access".to_string(),
-                    object: "object_type".to_string(),
-                }],
-                object: vec![],
-                object_type: vec![Permission {
-                    access: Access::allow,
-                    alias: "Access to FL".to_string(),
-                    id: None,
-                    level: PermissionLevel::object_type,
-                    kind: PermissionKind::create,
-                    name: "system_of_object_access".to_string(),
-                    object: "1".to_string(),
-                }],
-                object_type_field: vec![
-                    Permission {
-                        access: Access::allow,
-                        alias: "Access to Lastname".to_string(),
-                        id: None,
-                        level: PermissionLevel::object_type_field,
-                        kind: PermissionKind::create,
-                        name: "Access to Lastname".to_string(),
-                        object: "1".to_string(),
-                    },
-                    Permission {
-                        access: Access::allow,
-                        alias: "Access to Firstname".to_string(),
-                        id: None,
-                        level: PermissionLevel::object_type_field,
-                        kind: PermissionKind::create,
-                        name: "Access to Firstname".to_string(),
-                        object: "2".to_string(),
-                    },
-                    Permission {
-                        access: Access::allow,
-                        alias: "Access to Patronymic".to_string(),
-                        id: None,
-                        level: PermissionLevel::object_type_field,
-                        kind: PermissionKind::create,
-                        name: "Access to Patronymic".to_string(),
-                        object: "3".to_string(),
-                    },
-                    Permission {
-                        access: Access::allow,
-                        alias: "Access to birthday".to_string(),
-                        id: None,
-                        level: PermissionLevel::object_type_field,
-                        kind: PermissionKind::create,
-                        name: "Access to birthday".to_string(),
-                        object: "4".to_string(),
-                    },
-                ],
-                link: vec![],
-                link_type: vec![],
-            },
-        };
-        match block_on(Secure_Repository::createGroup(&g)) {
-            Ok(_) => {}
-            Err(e) => {
-                println!("{:?}", e);
-                panic!("Error create group");
-            }
-        };
-        vec![g]
-    }
-
-    pub fn add_test_user() {
-        match block_on(User_Repository::getUserByLogin("root".to_string())) {
-            Ok(_) => { return; }
-            Err(_) => {}
-        };
-
-        let user = User {
-            id: None,
-            login: "root".to_string(),
-            password: match bcrypt::hash("testestest".to_string(), DEFAULT_COST) {
-                Ok(h) => { h }
-                Err(e) => { panic!("Cannt hashed password"); }
-            },
-            access_token: "".to_string(),
-            oauth: "".to_string(),
-            groups: get_user_groups(),
-            date_last_active: None,
-            date_registred: Utc::now().naive_utc(),
-        };
-        let res = block_on(User_Repository::createUser(&user));
-        assert_eq!(res, Ok("1".to_string()));
-    }
-
-    fn login() -> String {
-        add_test_user();
-        let client = Client::tracked(rocket_build()).expect("valid rocket instance");
-        let mut response = client.post(uri!("/login")).body("{\
-            \"login\":\"root\",\
-            \"password\":\"testestest\"\
-        }").remote(SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080))
-            .dispatch();
-        let cookie_str = response
-            .headers()
-            .get("Set-Cookie")
-            .collect::<Vec<&str>>()
-            .pop()
-            .unwrap()
-            .to_string();
-        assert_eq!(response.status(), Status::Ok);
-        assert!(response.headers().get("Set-Cookie").count() >= 1);
-        assert_eq!(response.into_string(), Some("OK".to_string()));
-        return cookie_str;
-    }
-
 
     #[test]
     fn add_object_type_test() {
-        let session_cookie = login();
-        println!("Set cookie: '{}'", session_cookie.as_str());
-        let h = Header::new("Cookie", session_cookie);
+        let user = login();
+        // println!("Set cookie: '{}'", session_cookie.as_str());
+        // let h = Header::new("Cookie", session_cookie);
         let client = Client::tracked(rocket_build()).expect("valid rocket instance");
         let request = client.post(uri!("/link/add")).body("{\
                 \"fields\":[
@@ -321,7 +118,7 @@ mod test {
                 \"alias\":\"tl\"
             }");
         // request.add_header(h);
-        let cookie = CookieBuilder::new("user_id", "1").secure(true);
+        let cookie = CookieBuilder::new("user_id", user.id.unwrap()).secure(true);
         let response = request.private_cookie(cookie.finish()).dispatch();
 
 
@@ -330,13 +127,13 @@ mod test {
 
     #[test]
     fn get_object_type_test() {
-        let session_cookie = login();
-        println!("Set cookie: '{}'", session_cookie.as_str());
-        let h = Header::new("Cookie", session_cookie);
+        let user = login();
+        // println!("Set cookie: '{}'", session_cookie.as_str());
+        // let h = Header::new("Cookie", session_cookie);
         let client = Client::tracked(rocket_build()).expect("valid rocket instance");
         let request = client.get(uri!("/link/get/1"));
         // request.add_header(h);
-        let cookie = CookieBuilder::new("user_id", "1").secure(true);
+        let cookie = CookieBuilder::new("user_id", user.id.unwrap()).secure(true);
         let response = request.private_cookie(cookie.finish()).dispatch();
 
 
--- src/controllers/test/mod.rs
+++ src/controllers/test/mod.rs
@@ -16,8 +16,9 @@ use crate::model::user::entity::user::User;
 use crate::model::user::repository::repository::Repository as User_Repository;
 use crate::model::object::repository::repository::Repository as Object_Repository;
 use crate::model::secure::repository::repository::Repository as Secure_Repository;
+use rand::{distributions::Alphanumeric, Rng};
 
-fn add_object_type() {
+pub fn add_object_type() {
     match block_on(Object_Repository::getObjectTypeFromAlias("fl".to_string())) {
         Ok(_) => { return; }
         Err(_) => {}
@@ -83,16 +84,24 @@ fn add_object_type() {
 }
 
 
-fn get_user_groups() -> Vec<Group> {
-    let g = Group {
-        alias: "acc_object".to_string(),
+pub fn get_user_groups() -> Vec<Group> {
+    let mut g = Group {
+        alias: format!("group_of_{}_access_", rand::thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(7)
+            .map(char::from)
+            .collect::<String>()),
         name: "acc_object".to_string(),
         level: "system".to_string(),
         id: Some("1".to_string()),
         permissions: PermissionsGroup {
             system: vec![Permission {
                 access: Access::allow,
-                alias: "system_of_object_access".to_string(),
+                alias: format!("system_of_{}_access_", rand::thread_rng()
+                    .sample_iter(&Alphanumeric)
+                    .take(7)
+                    .map(char::from)
+                    .collect::<String>()),
                 id: None,
                 level: PermissionLevel::system,
                 kind: PermissionKind::create,
@@ -100,7 +109,11 @@ fn get_user_groups() -> Vec<Group> {
                 object: "object".to_string(),
             }, Permission {
                 access: Access::allow,
-                alias: "system_of_object_access".to_string(),
+                alias: format!("system_of_{}_access_", rand::thread_rng()
+                    .sample_iter(&Alphanumeric)
+                    .take(7)
+                    .map(char::from)
+                    .collect::<String>()),
                 id: None,
                 level: PermissionLevel::system,
                 kind: PermissionKind::create,
@@ -108,7 +121,11 @@ fn get_user_groups() -> Vec<Group> {
                 object: "user".to_string(),
             }, Permission {
                 access: Access::allow,
-                alias: "system_of_object_access".to_string(),
+                alias: format!("system_of_{}_access_", rand::thread_rng()
+                    .sample_iter(&Alphanumeric)
+                    .take(7)
+                    .map(char::from)
+                    .collect::<String>()),
                 id: None,
                 level: PermissionLevel::system,
                 kind: PermissionKind::create,
@@ -118,17 +135,29 @@ fn get_user_groups() -> Vec<Group> {
             object: vec![],
             object_type: vec![Permission {
                 access: Access::allow,
-                alias: "Access to FL".to_string(),
+                alias: format!("system_of_{}_access_", rand::thread_rng()
+                    .sample_iter(&Alphanumeric)
+                    .take(7)
+                    .map(char::from)
+                    .collect::<String>()),
                 id: None,
                 level: PermissionLevel::object_type,
                 kind: PermissionKind::create,
-                name: "system_of_object_access".to_string(),
+                name: format!("system_of_{}_access_", rand::thread_rng()
+                    .sample_iter(&Alphanumeric)
+                    .take(7)
+                    .map(char::from)
+                    .collect::<String>()),
                 object: "1".to_string(),
             }],
             object_type_field: vec![
                 Permission {
                     access: Access::allow,
-                    alias: "Access to Lastname".to_string(),
+                    alias: format!("system_of_{}_access_", rand::thread_rng()
+                        .sample_iter(&Alphanumeric)
+                        .take(7)
+                        .map(char::from)
+                        .collect::<String>()),
                     id: None,
                     level: PermissionLevel::object_type_field,
                     kind: PermissionKind::create,
@@ -137,7 +166,11 @@ fn get_user_groups() -> Vec<Group> {
                 },
                 Permission {
                     access: Access::allow,
-                    alias: "Access to Firstname".to_string(),
+                    alias: format!("system_of_{}_access_", rand::thread_rng()
+                        .sample_iter(&Alphanumeric)
+                        .take(7)
+                        .map(char::from)
+                        .collect::<String>()),
                     id: None,
                     level: PermissionLevel::object_type_field,
                     kind: PermissionKind::create,
@@ -146,7 +179,11 @@ fn get_user_groups() -> Vec<Group> {
                 },
                 Permission {
                     access: Access::allow,
-                    alias: "Access to Patronymic".to_string(),
+                    alias: format!("system_of_{}_access_", rand::thread_rng()
+                        .sample_iter(&Alphanumeric)
+                        .take(7)
+                        .map(char::from)
+                        .collect::<String>()),
                     id: None,
                     level: PermissionLevel::object_type_field,
                     kind: PermissionKind::create,
@@ -155,7 +192,11 @@ fn get_user_groups() -> Vec<Group> {
                 },
                 Permission {
                     access: Access::allow,
-                    alias: "Access to birthday".to_string(),
+                    alias: format!("system_of_{}_access_", rand::thread_rng()
+                        .sample_iter(&Alphanumeric)
+                        .take(7)
+                        .map(char::from)
+                        .collect::<String>()),
                     id: None,
                     level: PermissionLevel::object_type_field,
                     kind: PermissionKind::create,
@@ -168,7 +209,7 @@ fn get_user_groups() -> Vec<Group> {
         },
     };
     match block_on(Secure_Repository::createGroup(&g)) {
-        Ok(_) => {}
+        Ok(i) => { g.id = Some(i); }
         Err(e) => {
             println!("{:?}", e);
             panic!("Error create group");
@@ -177,18 +218,24 @@ fn get_user_groups() -> Vec<Group> {
     vec![g]
 }
 
-pub fn add_test_user() {
-    match block_on(User_Repository::getUserByLogin("root".to_string())) {
-        Ok(_) => { return; }
+pub fn add_test_user() -> User {
+    let login = format!("system_of_{}_access_", rand::thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(7)
+        .map(char::from)
+        .collect::<String>());
+
+    match block_on(User_Repository::getUserByLogin(login.clone())) {
+        Ok(u) => { return u; }
         Err(_) => {}
     };
 
-    let user = User {
+    let mut user = User {
         id: None,
-        login: "root".to_string(),
+        login,
         password: match bcrypt::hash("testestest".to_string(), DEFAULT_COST) {
             Ok(h) => { h }
-            Err(e) => { panic!("Cannt hashed password"); }
+            Err(e) => { panic!("Cannt hashed password {}", e); }
         },
         access_token: "".to_string(),
         oauth: "".to_string(),
@@ -197,16 +244,18 @@ pub fn add_test_user() {
         date_registred: Utc::now().naive_utc(),
     };
     let res = block_on(User_Repository::createUser(&user));
-    assert_eq!(res, Ok("1".to_string()));
+    assert_eq!(res.is_ok(), true);
+    user.id = Some(res.unwrap());
+    return user;
 }
 
-fn login() -> String {
-    add_test_user();
+pub fn login() -> User {
+    let user = add_test_user();
     let client = Client::tracked(rocket_build()).expect("valid rocket instance");
-    let mut response = client.post(uri!("/login")).body("{\
-            \"login\":\"root\",\
+    let mut response = client.post(uri!("/login")).body(format!("{{\
+            \"login\":\"{}\",\
             \"password\":\"testestest\"\
-        }").remote(SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080))
+        }}",user.login)).remote(SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080))
         .dispatch();
     let cookie_str = response
         .headers()
@@ -218,5 +267,5 @@ fn login() -> String {
     assert_eq!(response.status(), Status::Ok);
     assert!(response.headers().get("Set-Cookie").count() >= 1);
     assert_eq!(response.into_string(), Some("OK".to_string()));
-    return cookie_str;
+    return user;
 }
