ab0e0850eff4f05c59af7c8bb7505ed635e8049f
gorod <gorodmehov@gmail.com>
2022-07-03 03:42:55 +0300

    Рефакторинг тестов, небольшие правки по коду, спрятаны данные для входа

--- src/model/user/entity/user.rs
+++ src/model/user/entity/user.rs
@@ -3,7 +3,7 @@ use crate::model::secure::entity::permission::Group;
 use serde::{Serialize, Deserialize, Serializer};
 
 
-#[derive(Serialize, Deserialize, Debug,  Clone, PartialEq)]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 pub struct User {
     pub id: Option<String>,
     pub login: String,
@@ -14,17 +14,20 @@ pub struct User {
     #[serde(serialize_with = "User::ser_guard")]
     pub oauth: String,
     #[serde(serialize_with = "User::ser_groups_to_str")]
-    pub groups:Vec<Group>,
-    pub date_last_active:Option<NaiveDateTime>,
-    pub date_registred:NaiveDateTime
+    pub groups: Vec<Group>,
+    pub date_last_active: Option<NaiveDateTime>,
+    pub date_registred: NaiveDateTime,
 }
 
-impl User{
-    pub fn ser_groups_to_str<S: Serializer>(t:&Vec<Group>, s:S) -> Result<S::Ok, S::Error> {
-        let x = t.iter().map(|g| g.id.clone()).collect::<Vec<Option<String>>>();
-        x.serialize(s)
+impl User {
+    pub fn ser_groups_to_str<S: Serializer>(t: &Vec<Group>, s: S) -> Result<S::Ok, S::Error> {
+        t
+            .iter()
+            .map(|g| g.id.clone())
+            .collect::<Vec<Option<String>>>()
+            .serialize(s)
     }
-    pub fn ser_guard<S: Serializer>(t:&String, s:S) -> Result<S::Ok, S::Error> {
+    pub fn ser_guard<S: Serializer>(t: &String, s: S) -> Result<S::Ok, S::Error> {
         "hidden".serialize(s)
     }
 }
